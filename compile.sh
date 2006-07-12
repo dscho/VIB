e4b1f5afbf5bf6234a7b93957ca3b3352c98f5ee
gene099 <gene099>
2006-07-12 20:16:06 +0000

    fix compile of BatchProcessor_.java by providing imagescience.jar

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-javac -classpath ../ImageJ/ij.jar:jzlib-1.0.7.jar:Jama-1.0.2.jar $(find -name \*.java)
+javac -classpath ../ImageJ/ij.jar:jzlib-1.0.7.jar:Jama-1.0.2.jar:imagescience.jar $(find -name \*.java)
