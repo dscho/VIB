8890827277a58a3b258d5d493a86e7feb5f80181
gene099 <gene099>
2006-07-22 03:39:45 +0000

    fix typo

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 curdir="$(cd "$(dirname "$0")"; pwd)"
-java -jar "$curdir"/../ImageJ/ij.jar -Dplugins.dir "$curdir" "$@"
+java -jar "$curdir"/../ImageJ/ij.jar -Dplugins.dir="$curdir" "$@"
