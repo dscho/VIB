6238abfc2a06da1807c11847236855ceaf8dcf5b
Benjamin Schmid <bene@wbgn013.biozentrum.uni-wuerzburg.de>
2006-08-08 15:13:58 +0200

    Deleted some debugging code in AugmentedLandmarkWarp
    via git-CVS emulator

--- vib/AugmentedLandmarkWarp_.java
+++ vib/AugmentedLandmarkWarp_.java
@@ -85,7 +85,7 @@ public class AugmentedLandmarkWarp_ implements PlugInFilter {
 		for (int l = 0; l < labelCount; l++) {
 			if (matrix[l] == null)
 				continue;
-			//System.err.println("i: " + i + ", j: " + j + ", k: " + k + ", matrix[" + l + "]: " + matrix[l]);
+			
 			matrix[l].apply(i, j, k);
 			float x = matrix[l].x;
 			float y = matrix[l].y;
@@ -94,43 +94,14 @@ public class AugmentedLandmarkWarp_ implements PlugInFilter {
 			int xdiff = center[l][0] - i;
 			int ydiff = center[l][1] - j;
 			int zdiff = center[l][2] - k;
-			/*
-			int division = Math.abs(xdiff);
-			if (division < Math.abs(ydiff))
-				division = Math.abs(ydiff);
-			if (division < Math.abs(zdiff))
-				division = Math.abs(zdiff);
-			if (division == 0) {
-				this.x = x;
-				this.y = y;
-				this.z = z;
-				return;
-			}
-
-			int l0 = 0, l2 = division;
-			while (l2 > l0 + 1) {
-				int l1 = (l0 + l2) / 2;
-				int i2 = (int)(i + xdiff * l1 / division);
-				int j2 = (int)(j + ydiff * l1 / division);
-				int k2 = (int)(k + zdiff * l1 / division);
-
-				if (labels.getNoInterpol(i2, j2, k2) == l)
-					l2 = l1;
-				else
-					l0 = l1;
-			}
-
-			float factor = 1.0f / l2 / l2;
-			*/
+			
 			float factor = xdiff * xdiff + ydiff * ydiff + zdiff * zdiff;
 			factor = 1.0f / (factor + 0.01f);
-			//System.err.println("factor: " + factor);
-			//System.err.println("cumul: " + this.x + ", " + this.y + ", " + this.z);
-			//System.err.println("x: " + x + ", " + y + ", " + z);
+			
 			this.x += x * factor;
 			this.y += y * factor;
 			this.z += z * factor;
-			//System.err.println("cumul: " + this.x + ", " + this.y + ", " + this.z);
+			
 			total += factor;
 		}
 		x /= total;
@@ -140,26 +111,10 @@ public class AugmentedLandmarkWarp_ implements PlugInFilter {
 	}
 
 	void run() {
+
 		InterpolatedImage.Iterator iter = ii.iterator(true);
 		while (iter.next() != null) {
-			int value = labels.getNoInterpol(iter.i, iter.j, iter.k);
-			value = 0;
-			if (value > 0 && matrix[value] != null) {
-				matrix[value].apply(iter.i, iter.j, iter.k);
-				value = (int)model.interpol.get(
-						matrix[value].x,
-						matrix[value].y,
-						matrix[value].z);
-				ii.set(iter.i, iter.j, iter.k, value);
-				return;
-			}
-			//if (true) throw new RuntimeException("nonono");
-			if (false) {
-				ii.set(iter.i, iter.j, iter.k, 0);
-				return;
-			}
 			transCoord(iter.i, iter.j, iter.k);
-			///System.err.println("coords: " + x + ", " + y + ", " + z);
 			ii.set(iter.i, iter.j, iter.k, (int)model.interpol.get(x, y, z));
 		}
 		new AmiraParameters(model.image).setParameters(ii.image);
