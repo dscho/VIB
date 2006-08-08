a7b606d8c4fe6440863710f58b5e05c63b9762fd
Benjamin Schmid <bene@wbgn013.biozentrum.uni-wuerzburg.de>
2006-08-08 15:25:01 +0200

    New plugin for naming, saving and restoring points of an image
    via git-CVS emulator

---
+++ vib/Name_Points.java
@@ -0,0 +1,314 @@
+package vib;
+/**
+ * @author Benjamin Schmid
+ * 
+ * @date 07.08.2006
+ */
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.gui.GUI;
+import ij.gui.GenericDialog;
+import ij.gui.PointRoi;
+import ij.gui.Roi;
+import ij.io.FileInfo;
+import ij.plugin.PlugIn;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Dialog;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Label;
+import java.awt.MenuItem;
+import java.awt.Panel;
+import java.awt.Polygon;
+import java.awt.PopupMenu;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+
+class PointsDialog extends Dialog implements ActionListener {
+	
+	private PointList points;
+	private Name_Points plugin;	
+	private Panel panel;
+	private final PopupMenu popup = createPopup();
+	private NamedPoint current;
+	
+	public PointsDialog(String title,
+			    PointList points,
+			    Name_Points plugin) {
+		
+		super(IJ.getInstance(),title,false);
+		
+		this.add(popup);
+		this.plugin = plugin;
+		this.points = points;
+		
+		setLayout(new BorderLayout());
+		
+		Panel instructionsPanel = new Panel();
+		instructionsPanel.add(
+				new Label( "Mark the current point selection as:" ),
+				BorderLayout.WEST);
+		add(instructionsPanel,BorderLayout.NORTH);
+		
+		panel = new Panel();
+		updatePointsPanel();
+		add(panel,BorderLayout.CENTER);
+		
+		
+		Panel buttonsPanel = new Panel();
+		Button button = new Button("Load");
+		button.addActionListener(this);
+		buttonsPanel.add(button);
+		button = new Button("Add point");
+		button.addActionListener(this);
+		buttonsPanel.add(button);
+		button = new Button("Save");
+		button.addActionListener(this);
+		buttonsPanel.add(button);	
+		button = new Button("Reset");
+		button.addActionListener(this);
+		buttonsPanel.add(button);
+		button = new Button("Close");
+		button.addActionListener(this);
+		buttonsPanel.add(button);	
+		add(buttonsPanel,BorderLayout.SOUTH);
+	
+		GUI.center(this);
+		pack();
+		setVisible(true);
+	}
+	
+	private PopupMenu createPopup(){
+		PopupMenu popup = new PopupMenu();
+		MenuItem mi = new MenuItem("Rename point");
+		mi.addActionListener(this);
+		popup.add(mi);
+		mi = new MenuItem("Remove point");
+		mi.addActionListener(this);
+		popup.add(mi);
+		mi = new MenuItem("Reset point");
+		mi.addActionListener(this);
+		popup.add(mi);
+		return popup;
+	}
+	
+	public void updatePointsPanel(){
+		if(points.size() == 0)
+			addEmptyPoint();
+		panel.removeAll();
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		for (NamedPoint p : points) {
+			addRow(p,panel,c);
+		}
+		this.pack();
+	}
+	
+	private void addRow(final NamedPoint p, Panel panel, GridBagConstraints c){
+		c.gridx = 0;
+		c.gridy = GridBagConstraints.RELATIVE;
+		c.anchor = GridBagConstraints.LINE_START;			
+		final Button button = new Button(p.name);
+		button.addMouseListener(new MouseAdapter(){
+			public void mousePressed(MouseEvent e){
+				if(e.isPopupTrigger()){
+					current = p;
+					popup.show(button, e.getX(),e.getY());
+				}
+			}
+			public void mouseReleased(MouseEvent e){
+				if(e.isPopupTrigger()){
+					current = p;
+					popup.show(button, e.getX(),e.getY());
+				}
+			}
+		});
+		button.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				PointsDialog.this.plugin.mark(p);
+			}
+		});
+		panel.add(button,c);
+		
+		c.anchor = GridBagConstraints.LINE_START;
+		c.gridx = 1;
+		Label coordinateLabel = 
+			p.set ? new Label(p.coordinatesAsString())
+					: new Label("     <unset>     ");
+		panel.add(coordinateLabel,c);
+		
+		c.anchor = GridBagConstraints.LINE_START;
+		c.gridx = 2;
+		Button showB = new Button("Show");
+		showB.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				PointsDialog.this.plugin.show(p);
+			}
+		});
+		showB.setEnabled(p.set);
+		panel.add(showB,c);
+	}
+	
+	public void addEmptyPoint(){
+		NamedPoint p = new NamedPoint("point" + points.size());
+		points.add(p);
+		updatePointsPanel();
+	}
+	
+	public void removePoint(NamedPoint p){
+		points.remove(p);
+		updatePointsPanel();
+	}
+	
+	public void renamePoint(NamedPoint p){
+		GenericDialog gd = new GenericDialog("Rename point");
+		gd.addStringField("New name ", p.name);
+		gd.showDialog();
+		if(gd.wasCanceled())
+			return;
+		p.name = gd.getNextString();
+		updatePointsPanel();
+	}
+
+	public void resetPoint(NamedPoint p) {
+		p.set = false;
+		updatePointsPanel();
+	}
+
+	public void resetAll() {
+		for(NamedPoint p : points) {
+			p.set = false;
+		}
+		updatePointsPanel();
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		String command = e.getActionCommand();
+		if(command.equals("Close")){
+			dispose();
+		} else if (command.equals("Save")) {
+			plugin.save();
+		} else if (command.equals("Reset")) {
+			resetAll();
+		} else if (command.equals("Add point")){
+			addEmptyPoint();
+		} else if (command.equals("Rename point")){
+			renamePoint(current);
+		} else if (command.equals("Remove point")){
+			removePoint(current);
+		} else if (command.equals("Reset point")){
+			resetPoint(current);
+		} else if (command.equals("Load")){
+			PointsDialog.this.plugin.load();
+		} 
+	}
+}
+
+public class Name_Points implements PlugIn {
+
+	public void show(NamedPoint p) {
+		if(!p.set){
+			IJ.error("Point is not set yet");
+			return;
+		}
+		int slice = (int)p.z;
+		if(slice < 0)
+			slice = 0;
+		if(slice > imp.getStackSize())
+			slice = imp.getStackSize()-1;
+		imp.setSlice(slice+1);
+		Roi roi	= new PointRoi((int)p.x,
+				       (int)p.y,
+				       imp);
+		imp.setRoi(roi);
+	}
+	
+	public void load() {
+		PointList newNamedPoints = PointList.load(imp);
+		
+		if(newNamedPoints==null)
+			return;
+
+		for (NamedPoint current : newNamedPoints) {
+			boolean foundName = false;
+			for(NamedPoint p : points) {
+				if (current.name.equals(p.name)) {
+					p.set(current.x, current.y, current.z);
+					p.set = true;
+					foundName = true;
+				}
+			}
+			if (!foundName)
+				points.add(current);
+		}
+		dialog.updatePointsPanel();
+	}
+	
+	public void save() {
+
+		FileInfo info = imp.getOriginalFileInfo();
+		if( info == null ) {
+			IJ.error("There's no original file name that these points refer to.");
+			return;
+		}
+		String fileName = info.fileName + ".points";
+		String directory = info.directory;
+		IJ.showStatus("Saving point annotations to "+ directory + fileName);
+		points.save(directory, fileName);
+		IJ.showStatus("Saved point annotations.");
+	}
+
+	public void mark(NamedPoint point) {
+		Roi roi = imp.getRoi();
+		if (roi!=null && roi.getType()==Roi.POINT) {
+			Polygon p = roi.getPolygon();
+			
+			if(p.npoints > 1) {
+				IJ.error("You can only have one point selected to mark.");
+				return;
+			}
+
+			int x = p.xpoints[0];
+			int y = p.ypoints[0];
+			int z = imp.getCurrentSlice()-1;
+
+			point.set(x,y,z);
+			point.set = true;
+			dialog.updatePointsPanel();
+
+		} else {
+			IJ.error("You must have a current point selection in "+
+				 imp.getTitle()+" in order to mark points.");
+		}
+	}
+
+	PointsDialog dialog;
+	ImagePlus imp;	
+	PointList points;
+
+	public void run(String arg) {
+		/*
+		String test1 = "one backslash '\\' and one double quote '\"'";
+		System.out.println("escaping: "+test1);
+		System.out.println("gives: "+escape(test1));
+		*/
+
+		imp = IJ.getImage();
+		
+		if(imp == null)
+			IJ.error("There's no image to annotate.");
+
+		points = new PointList();
+
+		dialog = new PointsDialog("Marking up: "+imp.getTitle(),
+					points,
+					this);
+
+	}
+}
---
+++ vib/PointList.java
@@ -0,0 +1,162 @@
+package vib;
+import ij.IJ;
+import ij.ImagePlus;
+import ij.io.FileInfo;
+import ij.io.OpenDialog;
+import ij.io.SaveDialog;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+/**
+ * @date 07.08.2006
+ * 
+ * @author Benjamin Schmid
+ */
+class PointList implements Iterable<NamedPoint>{
+	
+	private List<NamedPoint> points;
+	
+	public PointList(){
+		points = new ArrayList<NamedPoint>();
+	}
+		
+	public void add(NamedPoint point){
+		points.add(point);
+	}
+	
+	public void remove(NamedPoint point){
+		points.remove(point);
+	}
+	
+	public void rename(NamedPoint point, String name){
+		point.name = name;
+	}
+	
+	public NamedPoint get(int index){
+		return points.get(index);
+	}
+	
+	public int size(){
+		return points.size();
+	}
+	
+	public NamedPoint get(String name){
+		for(NamedPoint p : points){
+			if(p.name.equals(name)){
+				return p;
+			}
+		}
+		return null;
+	}
+	
+	public Iterator<NamedPoint> iterator() {
+		return points.iterator();
+	}
+	
+	public static PointList load(ImagePlus imp){
+		FileInfo info = imp.getOriginalFileInfo();
+		if(info != null){
+			PointList l =  load(info.directory,info.fileName + ".points",false);
+			if(l == null){
+				l = load(info.directory,info.fileName + ".points",true);
+			}
+			return l;
+		}
+		return null;
+	}
+	
+	public static PointList load(String dir, String file, boolean showDialog){
+
+		String openPath = dir + File.separatorChar + file;
+		if(showDialog){
+			OpenDialog od = new OpenDialog("Open points annotation file",
+					dir,file);
+			
+			
+			if(od.getFileName()==null)
+				return null;
+			else {
+				openPath = od.getDirectory()+od.getFileName();
+			}
+		}
+				
+		PointList list = new PointList();	
+		try {
+			BufferedReader f = new BufferedReader(
+							new FileReader(openPath));
+			String line;
+			while ((line=f.readLine())!=null) {
+				NamedPoint p = NamedPoint.fromLine(line);
+				if(p != null)
+					list.add(p);
+			}
+			return list;
+		} catch (FileNotFoundException e) {
+			IJ.showMessage("Could not find file " + openPath);
+		} catch (IOException e) {
+			IJ.showMessage("Could not read file " + openPath);
+		}
+		return null;
+	}
+	
+	public void save(String directory, String fileName ) {
+
+		String suggestedSaveFilename = fileName+".points";
+
+		SaveDialog sd = new SaveDialog("Save points annotation file as...",
+					       directory,
+					       suggestedSaveFilename,
+					       ".points");
+
+		if(sd.getFileName()==null)
+			return;
+		
+		String savePath = sd.getDirectory()+sd.getFileName();
+		File file = new File(savePath);
+		if ((file != null) && file.exists()) {
+			if (!IJ.showMessageWithCancel(
+				    "Save points annotation file", "The file "+
+				    savePath+" already exists.\n"+
+				    "Do you want to replace it?"))
+				return;
+		}
+		IJ.showStatus("Saving point annotations to "+savePath);
+
+		try {
+			PrintStream fos = new PrintStream(savePath);
+			for(NamedPoint p : points){
+				if(p.set) {
+					fos.println(p.toYAML() + "\n");
+				}
+			}
+			fos.close();
+		} catch( IOException e ) {
+			IJ.error("Error saving to: "+savePath+"\n"+e);
+		}				
+		IJ.showStatus("Saved point annotations.");
+	}
+	
+	public static ArrayList<String> pointsInBoth(PointList points0,
+		     PointList points1) {
+
+		ArrayList<String> common = new ArrayList<String>();
+		for(NamedPoint point0 : points0){
+			for(NamedPoint point1 : points1){
+				if(point0.name.equals(point1.name)){
+					common.add(point0.name);
+					break;
+				}
+			}
+		}
+		return common;
+	}
+}
