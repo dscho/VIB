e35750aa3710000f9e6ec5a62bcb2d82a7cf127a
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2006-08-16 13:54:12 +0200

    fix for cygwin

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 curdir="$(cd "$(dirname "$0")"; pwd)"
+case `uname -o 2>/dev/null`
+in Cygwin*) curdir=$(cygpath --mixed $curdir);;
+esac
+
 java -Dplugins.dir="$curdir" -jar "$curdir"/../ImageJ/ij.jar "$@"
