71368ecff201e3c6529bd2abbd35b0b3d1d28ec5
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2006-10-05 02:40:26 +0200

    implement the forward transformation reproducing the results of the paper

--- Moving_Least_Squares.java
+++ Moving_Least_Squares.java
@@ -63,6 +63,7 @@ public class Moving_Least_Squares implements PlugInFilter {
 		gd.addChoice("source", titles, titles[0]);
 		gd.addNumericField("alpha", 1.0, 3);
 		gd.addNumericField("gridSize", 0.0, 3);
+		gd.addCheckbox("forward", true);
 		gd.showDialog();
 		if (gd.wasCanceled())
 			return;
@@ -79,18 +80,26 @@ public class Moving_Least_Squares implements PlugInFilter {
 		id = ids[gd.getNextChoiceIndex()];
 		method.alpha = (float)gd.getNextNumber();
 		float gridSize = (float)gd.getNextNumber();
+		boolean useForward = gd.getNextBoolean();
 
 		ImagePlus source = WindowManager.getImage(id);
 		Interpolator inter =
 			new BilinearInterpolator(source.getProcessor());
 
 		PointRoi points2 = (PointRoi)source.getRoi();
-		method.setCoordinates(points1, points2);
 
-		int w = ip.getWidth(), h = ip.getHeight();
-		method.warpImage(inter, w, h, ip.getPixels());
-		if (gridSize > 0)
-			method.drawGrid(w, h, gridSize, ip.getPixels());
+		if (useForward) {
+			if (gridSize < 1)
+				gridSize = 10;
+			method.setCoordinates(points2, points1);
+			method.warpImageForward(inter, (int)gridSize, ip);
+		} else {
+			method.setCoordinates(points1, points2);
+			int w = ip.getWidth(), h = ip.getHeight();
+			method.warpImage(inter, w, h, ip.getPixels());
+			if (gridSize > 0)
+				method.drawGrid(w, h, gridSize, ip.getPixels());
+		}
 		image.updateAndDraw();
 	}
 
@@ -100,6 +109,9 @@ public class Moving_Least_Squares implements PlugInFilter {
 	}
 
 	static abstract class Method {
+		// an alpha of 1 corresponds to Euclidean distance
+		public float alpha = 1.0f;
+
 		int n;
 		// the centroids
 		float pCX, pCY, qCX, qCY;
@@ -134,8 +146,6 @@ public class Moving_Least_Squares implements PlugInFilter {
 				qY[i] = y2[i] + oY2;
 			}
 		}
-
-		public float alpha = 1.0f;
 
 		// both x, y and px, py are supposed to be absolute
 		public float w(float x, float y, float px, float py) {
@@ -291,6 +301,151 @@ public class Moving_Least_Squares implements PlugInFilter {
 						pixels[i + w * j] = 0;
 				}
 				IJ.showProgress(j + 1, h);
+			}
+		}
+
+		/**
+		 * Transform the given quadrilateral by iterating through
+		 * the integer coordinates of the target, and bilinearly
+		 * interpolating in the source.
+		 * This is implemented quick 'n dirty.
+		 */
+		void forwardQuadrilateral(Interpolator source,
+				float[] sX, float[] sY,
+				ImageProcessor target,
+				int w, int h,
+				float[] tX, float[] tY) {
+			float minY, maxY;
+			minY = maxY = tY[0];
+			for (int i = 1; i < 4; i++)
+				if (minY > tY[i])
+					minY = tY[i];
+				else if (maxY < tY[i])
+					maxY = tY[i];
+			int startY = minY < 0 ? 0 : (int)Math.floor(minY);
+			int stopY = maxY > h ? h : (int)Math.ceil(maxY);
+			for (int y = startY; y < stopY; y++) {
+				// find intersecting edges
+				float minX = w, maxX = 0;
+				for (int i = 0; i < 4; i++) {
+					int i1 = i == 3 ? 0 : i + 1;
+					float y1 = (float)Math.round(tY[i]);
+					float y2 = (float)Math.round(tY[i1]);
+					float rY = (float)Math.round(y);
+					if ((rY - y1) * (rY - y2) > 0)
+						continue;
+					if (y1 != y2) {
+						float x = tX[i] + (y - tY[i]) *
+							(tX[i1] - tX[i]) /
+							(tY[i1] - tY[i]);
+						if (minX > x)
+							minX = x;
+						if (maxX < x)
+							maxX = x;
+						continue;
+					}
+					if (tX[i] < tX[i1]) {
+						if (minX > tX[i])
+							minX = tX[i];
+						if (maxX < tX[i1])
+							maxX = tX[i1];
+					} else {
+						if (minX > tX[i1])
+							minX = tX[i1];
+						if (maxX < tX[i])
+							maxX = tX[i];
+					}
+				}
+				// now interpolate
+				int startX = minX < 0 ? 0
+					: (int)Math.floor(minX);
+				int stopX = maxX > w ? w
+					: (int)Math.ceil(maxX);
+				for (int x = startX; x < stopX; x++) {
+					float a, b, c, d, e, f, g, s;
+					a = tX[0] - x;
+					b = tX[1] - tX[0];
+					c = tX[3] - tX[0];
+					d = tX[2] - tX[3] - tX[1] + tX[0];
+					e = tY[0] - y;
+					f = tY[1] - tY[0];
+					g = tY[3] - tY[0];
+					s = tY[2] - tY[3] - tY[1] + tY[0];
+					float p, q, r, dx, dy;
+					p = b * s - d * f;
+					q = b * g + a * s - d * e - c * f;
+					r = a * g - c * e;
+					float D = q * q - 4 * p * r;
+					if (p == 0 || D < 0) {
+						if (b == 0) {
+							dx = 0;
+							dy = -e / g;
+						} else {
+							dx = -a / c;
+							dy = 0;
+						}
+					} else {
+						dx = (-q + (float)Math.sqrt(D))
+							/ 2 / p;
+						dy = -(a + b * dx)
+							/ (c + d * dx);
+					}
+
+					float x0 = (1 - dx) * (1 - dy) * sX[0]
+						+ dx * (1 - dy) * sX[1]
+						+ (1 - dx) * dy * sX[3]
+						+ dx * dy * sX[2];
+					float y0 = (1 - dx) * (1 - dy) * sY[0]
+						+ dx * (1 - dy) * sY[1]
+						+ (1 - dx) * dy * sY[3]
+						+ dx * dy * sY[2];
+					float value = source.get(x0, y0);
+					target.setf(x, y, value);
+				}
+			}
+		}
+
+		// this is quick 'n dirty
+		public void warpImageForward(Interpolator source,
+				int gridSize, ImageProcessor target) {
+			boolean drawGrid = false;
+			int w = target.getWidth(), h = target.getHeight();
+			if (drawGrid)
+				target.setValue(0);
+			for (int y = 0; y < source.h; y += gridSize) {
+				for (int x = 0; x < source.w; x += gridSize) {
+					float[] sX = new float[4];
+					float[] sY = new float[4];
+					float[] tX = new float[4];
+					float[] tY = new float[4];
+					sX[0] = x; sY[0] = y;
+					sX[1] = x + gridSize; sY[1] = y;
+					sX[2] = x + gridSize;
+					sY[2] = y + gridSize;
+					sX[3] = x; sY[3] = y + gridSize;
+					for (int i = 0; i < 4; i++) {
+						calculate(sX[i], sY[i]);
+						tX[i] = resultX;
+						tY[i] = resultY;
+					}
+					forwardQuadrilateral(source, sX, sY,
+							target, w, h, tX, tY);
+					if (drawGrid) {
+						target.drawLine(
+							(int)tX[0], (int)tY[0],
+							(int)tX[1], (int)tY[1]);
+						target.drawLine(
+							(int)tX[1], (int)tY[1],
+							(int)tX[2], (int)tY[2]);
+						target.drawLine(
+							(int)tX[2], (int)tY[2],
+							(int)tX[3], (int)tY[3]);
+						target.drawLine(
+							(int)tX[3], (int)tY[3],
+							(int)tX[0], (int)tY[0]);
+					}
+				}
+				IJ.showProgress(y + gridSize, source.h);
 			}
 		}
 	}
