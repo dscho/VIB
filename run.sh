4124d1cb42579fe49bf41f6aefe939c34278ec28
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2006-12-04 18:30:42 +0100

    make work with cygwin

--- run.sh
+++ run.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
 curdir="$(cd "$(dirname "$0")"; pwd)"
-case `uname -o 2>/dev/null`
-in Cygwin*) curdir=$(cygpath --mixed $curdir);;
+case `uname -o 2>/dev/null` in
+Cygwin*)
+	curdir=$(cygpath --mixed $curdir)
+	CPSEP=\;
+	;;
+*)
+	CPSEP=:
+	;;
 esac
 
-java -Xmx256m -Dplugins.dir="$curdir" -jar "$curdir"/../ImageJ/ij.jar "$@"
+case "$1" in
+app)
+	shift
+	java -Xmx256m -Dplugins.dir="$curdir" \
+		-classpath "$curdir"/../ImageJ/ij.jar$CPSEP. vib.app.App "$@"
+	;;
+*)
+	java -Xmx256m -Dplugins.dir="$curdir" \
+		-jar "$curdir"/../ImageJ/ij.jar "$@"
+	;;
+esac
+
