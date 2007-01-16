15c8d5d7f441da264a7a829cd07e0ed28ea5a0b0
Johannes Schindelin <johannes.schindelin@gmx.de>
2007-01-16 01:28:47 +0100

    a better approach for the PriorityQueue; start partial calculation

--- Lasso_.java
+++ Lasso_.java
@@ -5,6 +5,8 @@ import ij.process.FloatProcessor;
 import ij.process.ImageProcessor;
 import ij.plugin.filter.PlugInFilter;
 
+import java.util.TreeMap;
+
 public class Lasso_ implements PlugInFilter{
 	private byte[] pixels;
 	private int[] dijkstra;
@@ -17,6 +19,7 @@ public class Lasso_ implements PlugInFilter{
 
 		pixels = (byte[])ip.getPixels();
 		initDijkstra(100, 100);
+		getDijkstra(120, 150);
 		new ImagePlus("Dijkstra", new FloatProcessor(w, h, dijkstra)).show();
 	}
 
@@ -24,103 +27,77 @@ public class Lasso_ implements PlugInFilter{
 		return pixels[x + w * y] & 0xff;
 	}
 
-	final static int[] stepX = { -1, 0, 1, 1, 1, 0, -1, -1 };
-	final static int[] stepY = { -1, -1, -1, 0, 1, 1, 1, 0 };
-	final static int[] stepW = { 4, 3, 4, 3, 4, 3, 4, 3 };
+	private class PixelCost implements Comparable {
+		int x, y, cost;
 
-	private class PixelQueue {
-		private int[] xQueue, yQueue, cost;
-		private boolean[] pixelInQueue;
-		private int i;
-
-		public int x, y;
-
-		public PixelQueue() {
-			xQueue = new int[w * h];
-			yQueue = new int[w * h];
-			cost = new int[w * h];
-			pixelInQueue = new boolean[w * h];
-			i = 0;
+		public PixelCost(int x, int y, int cost) {
+			this.x = x;
+			this.y = y;
+			this.cost = cost;
 		}
 
-		public int size() {
-			return i;
+		public int compareTo(Object other) {
+			PixelCost o = (PixelCost)other;
+			if (cost != o.cost)
+				return cost - o.cost;
+			if (x != o.x)
+				return x - o.x;
+			return y - o.y;
+		}
+	}
+
+	private class PixelQueue extends TreeMap {
+		public int x, y, cost;
+
+		public int getNextCost() {
+			if (size() > 0) {
+				PixelCost c = (PixelCost)firstKey();
+				return c.cost;
+			}
+			return Integer.MAX_VALUE;
 		}
 
 		public void push(int x, int y, int cost) {
-			if (pixelInQueue[x + w * y]) {
-				int index = findIndex(x, y);
-				if (this.cost[index] < cost)
-					return;
-				remove(index);
+			PixelCost c = new PixelCost(x, y, cost);
+			put(c, c);
+		}
+
+		public boolean pop() {
+			while (size() > 0) {
+				PixelCost c = (PixelCost)firstKey();
+				remove(c);
+				if (dijkstra[c.x + w * c.y] > c.cost) {
+					x = c.x;
+					y = c.y;
+					cost = c.cost;
+					return true;
+				}
 			}
-			int index = binarySearch(cost);
-			if (index < i)
-				insertAt(index);
-			else
-				i++;
-			xQueue[index] = x;
-			yQueue[index] = y;
-			this.cost[index] = cost;
-			pixelInQueue[x + w * y] = true;
-		}
-
-		public void pop() {
-			i--;
-			x = xQueue[i];
-			y = yQueue[i];
-			pixelInQueue[x + w * y] = false;
-		}
-
-		private void insertAt(int index) {
-			System.arraycopy(xQueue, index, xQueue, index + 1, i - index);
-			System.arraycopy(yQueue, index, yQueue, index + 1, i - index);
-			System.arraycopy(cost, index, cost, index + 1, i - index);
-			i++;
-		}
-
-		private void remove(int index) {
-			i--;
-			System.arraycopy(xQueue, index + 1, xQueue, index, i - index);
-			System.arraycopy(yQueue, index + 1, yQueue, index, i - index);
-			System.arraycopy(cost, index + 1, cost, index, i - index);
-		}
-
-		private int findIndex(int x, int y) {
-			for (int j = 0; j < i; j++)
-				if (xQueue[j] == x && yQueue[j] == y)
-					return j;
-			return -1;
-		}
-
-		private int binarySearch(int cost) {
-			int i1 = -1, i2 = i;
-			while (i1 + 1 < i2) {
-				int i3 = (i1 + i2) / 2;
-				if (this.cost[i3] >= cost)
-					i1 = i3;
-				else
-					i2 = i3;
-			}
-			return i1 + 1;
+			return false;
 		}
 	}
+
+	final static int[] stepX = { -1, 0, 1, 1, 1, 0, -1, -1 };
+	final static int[] stepY = { -1, -1, -1, 0, 1, 1, 1, 0 };
+	final static int[] stepW = { 4, 3, 4, 3, 4, 3, 4, 3 };
+	PixelQueue queue;
 
 	private void initDijkstra(int x, int y) {
 		dijkstra = new int[w * h];
 		for (int i = 0; i < w * h; i++)
 			dijkstra[i] = Integer.MAX_VALUE;
 
-		PixelQueue queue = new PixelQueue();
+		queue = new PixelQueue();
 		queue.push(x, y, 0);
-		dijkstra[x + w * y] = 0;
+	}
 
-		int nr = 0, total = w * h;
-		while (queue.size() > 0) {
-			queue.pop();
-			x = queue.x;
-			y = queue.y;
-			int weight = dijkstra[x + w * y];
+	private void getDijkstra(int x_, int y_) {
+		while (queue.getNextCost() < dijkstra[x_ + w * y_]
+				&& queue.pop()) {
+			int x = queue.x;
+			int y = queue.y;
+			int cost = queue.cost;
+			dijkstra[x + w * y] = cost;
 			int value = get(x, y);
 
 			for (int i = 0; i < stepW.length; i++) {
@@ -129,18 +106,10 @@ public class Lasso_ implements PlugInFilter{
 
 				if (x2 < 0 || y2 < 0 || x2 >= w || y2 >= h)
 					continue;
-				int cost = Math.abs(get(x2, y2) - value);
-				int newW = weight + stepW[i] * cost;
-				if (dijkstra[x2 + w * y2] > newW) {
-					queue.push(x2, y2, newW);
-
-					if (dijkstra[x2 + w * y2] == Integer.MAX_VALUE) {
-						nr++;
-						if ((nr % 100) == 0)
-							IJ.showProgress(nr, total);
-					}
-					dijkstra[x2 + w * y2] = newW;
-				}
+				int newC = cost + stepW[i]
+					* Math.abs(get(x2, y2) - value);
+				if (dijkstra[x2 + w * y2] > newC)
+					queue.push(x2, y2, newC);
 			}
 		}
 	}
