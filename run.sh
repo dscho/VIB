dd83c0b787e1a851548447442034ad566c2d0b0e
Johannes Schindelin <johannes.schindelin@gmx.de>
2007-02-13 21:03:13 +0100

    on my laptop, OpenGL pbuffer is broken for the time being

--- run.sh
+++ run.sh
@@ -11,14 +11,19 @@ Cygwin*)
 	;;
 esac
 
+case $(hostname) in
+racer)
+	EXTRADEFS="$EXTRADEFS -Dj3d.noOffScreen=true"
+esac
+
 case "$1" in
 app)
 	shift
-	java -Xmx256m -Dplugins.dir="$curdir" \
+	java -Xmx256m -Dplugins.dir="$curdir" $EXTRADEFS \
 		-classpath "$curdir"/../ImageJ/ij.jar$CPSEP. vib.app.App "$@"
 	;;
 *)
-	java -Xmx256m -Dplugins.dir="$curdir" \
+	java -Xmx256m -Dplugins.dir="$curdir" $EXTRADEFS \
 		-jar "$curdir"/../ImageJ/ij.jar "$@"
 	;;
 esac
