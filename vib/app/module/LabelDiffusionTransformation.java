48e352879a7286e3faaadfedc02412bdbab65519
Johannes Schindelin <johannes.schindelin@gmx.de>
2007-03-12 03:34:15 +0100

    converted LabelDiffusionTransformation

--- vib/app/module/LabelDiffusionTransformation.java
+++ vib/app/module/LabelDiffusionTransformation.java
@@ -1,153 +1,94 @@
 package vib.app.module;
 
-import java.util.Hashtable;
-import java.util.List;
-import java.util.ArrayList;
-import java.io.File;
-import ij.IJ;
 import ij.ImagePlus;
-import ij.text.TextPanel;
-import ij.util.Tools;
-import vib.RigidRegistration_;
-import vib.InterpolatedImage;
-import vib.TransformedImage;
-import vib.app.VIBImage;
+
+import vib.app.ImageMetaData;
 import vib.app.Options;
-import amira.AmiraTable;
-import vib.Center_Transformation;
+import vib.app.State;
+
 import vib.DiffusionInterpol2_;
 import vib.FloatMatrix;
-import distance.TwoValues;
+import vib.InterpolatedImage;
+import vib.RigidRegistration_;
+import vib.TransformedImage;
 
 public class LabelDiffusionTransformation extends Module {
+	protected final String name = "LabelDiffusionTransformation";
+	protected final String message =
+		"Calculating label diffusion transformation";
 
-	private VIBImage template;
-	
-	public LabelDiffusionTransformation(VIBImage imp, Options options) {
-		super(imp, options, true);
-		this.template = image.getTemplate();
-		dependingOn.add(LabelCenterTransformation.class);
-	}
+	protected static void run(State state, int index) {
+		LabelCenterTransformation.runOnOneImage(state, index);
 
-	public String getName() {
-		return "Calculating label diffusion transformation";
-	}
+		ImagePlus templateLabels = null;
 
-	public int checkResults() {
-		// check availability
-		File statistics = new File(image.statisticsPath);
-		File warped = null;
-		boolean uptodate = true;
-		for(int i=0; i<options.getNumChannels(); i++) {
-			warped = new File(image.getWarpedPath(i+1));
-			if(!warped.exists()) {
-				return RESULTS_UNAVAILABLE;
-			}
-			if(warped.lastModified() == 0L || 
-				warped.lastModified() < statistics.lastModified()) {
-				uptodate = false;
-			}
-		}
-		warped = new File(image.warpedLabelsPath);
-		if(!warped.exists()) {
-			return RESULTS_UNAVAILABLE;
-		}
-		if(warped.lastModified() == 0L || 
-			warped.lastModified() < statistics.lastModified()) {
-			uptodate = false;
-		}
-		if(!uptodate)
-			return RESULTS_OUT_OF_DATE;
-		return RESULTS_OK;
-	}
-
-	private FloatMatrix[] readTransformations() {
-		AmiraTable modelStatistics = image.getStatistics();
-		modelStatistics.hide();
-
-		Hashtable modelH = 
-			(Hashtable)modelStatistics.getProperties().get("Parameters");
-		TextPanel panel = modelStatistics.getTextPanel();
-		int count = panel.getLineCount();
-		FloatMatrix[] transformations = new FloatMatrix[count];
-		/*
-		 * DiffusionInterpol2_ needs an array of transformations with 
-		 * an entry for _ALL_ materials, even if they are not labelled.
-		 * Then, the transformation is just null. The same holds true 
-		 * for 'Exterior'
-		 */
-		transformations[0] = null;
-		for (int i = 1; i < count; i++) {
-			String[] line = Tools.split(panel.getLine(i), "\t");
-			String materialName = line[1];
-			int material = i;
-			// check if labelfield is empty:
-			int voxelCount = Integer.parseInt(line[2]);
-			if(voxelCount == 0) {
-				transformations[i] = null;
-				continue;
-			}
-			String key = template.basename
-							+ "SLabelTransformation-" + materialName;
-			String value = (String)modelH.get(key);
-			if(value == null)
-				console.append("transformation for material " + materialName + 
-						" could not be read");
-			transformations[i] = FloatMatrix.parseMatrix(value);
-		}
-
-		return transformations;
-	}
-
-	private FloatMatrix[] copyMatrices(FloatMatrix[] orig) {
-		FloatMatrix[] res = new FloatMatrix[orig.length];
-		for(int i = 0; i < orig.length; i++) {
-			if(orig[i] == null)
-				res[i] = null;
-			else 
-				res[i] = new FloatMatrix(orig[i]);
-		}
-		return res;
-	}
-	
-	protected void runThisModule() {
-		ImagePlus templateLabels = template.getResampledLabels();
-		// workaround: Labelfields have a wrong Calibration
-		templateLabels.setCalibration(
-				template.getResampledReferenceChannel().getCalibration());
-		// duplicate
-		ImagePlus scratch = new InterpolatedImage(templateLabels)
-										.cloneDimensionsOnly().getImage();
 		DiffusionInterpol2_ interpol = new DiffusionInterpol2_();
 		boolean rememberDistortion = true;
 		boolean reuseDistortion = false;
 		float tolerance = 0.5f;
-		FloatMatrix[] transformations = readTransformations();
+
+		FloatMatrix[] transformations = null;
 
 		// DiffusionInterpolation for all channels
-		for(int i=0; i<options.getNumChannels(); i++) {
-			ImagePlus model = image.getResampledChannel(i+1);
-			// copy transformations because they get transformed in 
-			// DiffusionInterpol2_.
+		for(int i = -1; i < state.options.numChannels; i++) {
+			String imagePath = state.getResampledPath(i, index);
+			String warpedPath = state.getWarpedPath(i, index);
+			String statisticsPath = state.getStatisticsPath(index);
+			if (state.upToDate(new String[] { imagePath,
+					statisticsPath }, warpedPath))
+				continue;
+
+			if (templateLabels == null)
+				templateLabels = state.getTemplateLabels();
+			InterpolatedImage ii =
+				new InterpolatedImage(templateLabels);
+			ImagePlus scratch =
+				ii.cloneDimensionsOnly().getImage();
+
+			if (transformations == null)
+				transformations =
+					readTransformations(state, index);
+
+			ImagePlus model = state.getImage(imagePath);
+
+			/*
+			 * Copy transformations because they get transformed
+			 * in DiffusionInterpol2_.
+			 */
 			FloatMatrix[] trans = copyMatrices(transformations);
-			interpol.initialize(scratch, templateLabels, model, trans, 
-					reuseDistortion, rememberDistortion, tolerance);
+			interpol.initialize(scratch, templateLabels, model,
+					trans,
+					reuseDistortion, rememberDistortion,
+					tolerance);
 			interpol.doit();
 			reuseDistortion = true; // true after the first channel
-			if(!image.saveWarped(i+1, scratch))
-				console.append("could not save " + image.getWarpedPath(i+1));
+			state.save(scratch, warpedPath);
 		}
 
-		// DiffusionInterpolation for labels
-		ImagePlus model = image.getResampledLabels();
-		// workaround for wrong calibration of Labelfields
-		model.setCalibration(
-				image.getResampledReferenceChannel().getCalibration());
-		FloatMatrix[] trans = copyMatrices(transformations);
-		interpol.initialize(scratch, templateLabels, model, trans, 
-				reuseDistortion, rememberDistortion, tolerance);
-		interpol.doit();
-		if(!image.saveWarpedLabels(scratch))
-			console.append("could not save " + image.warpedLabelsPath);
+	}
+
+	private static FloatMatrix[] readTransformations(State state,
+			int index) {
+		ImageMetaData templStats = state.getStatistics(-1);
+		ImageMetaData stats = state.getStatistics(index);
+
+		FloatMatrix[] result =
+			new FloatMatrix[templStats.materials.length];
+		for (int i = 0; i < result.length; i++) {
+			String name = templStats.materials[i].name;
+			String transformName = state.getTransformLabel() +
+				name;
+			result[i] = stats.getMatrix(transformName);
+			if (result[i] == null)
+				result[i] = new FloatMatrix(1.0f);
+		}
+
+		return result;
+	}
+
+	private static FloatMatrix[] copyMatrices(FloatMatrix[] orig) {
+		FloatMatrix[] res = new FloatMatrix[orig.length];
+		System.arraycopy(orig, 0, res, 0, orig.length);
+		return res;
 	}
 }
