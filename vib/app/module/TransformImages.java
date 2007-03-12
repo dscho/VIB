8694738b8025e3f1e659446cbeebefe8d0995dc5
Johannes Schindelin <johannes.schindelin@gmx.de>
2007-03-12 01:35:55 +0100

    finish TransformImages

--- vib/app/module/TransformImages.java
+++ vib/app/module/TransformImages.java
@@ -1,6 +1,24 @@
 package vib.app.module;
 
+import vib.app.Options;
+import vib.app.State;
+
 public class TransformImages extends Module {
-	// TODO
+	protected final String name = "TransformImages";
+	protected final String message = "Transforming images";
+
+	public static void run(State state, int index) {
+		Module module = null;
+		switch (state.options.transformationMethod) {
+		case Options.CENTER:
+			module = new LabelCenterTransformation();
+			break;
+		case Options.LABEL_DIFFUSION:
+			module = new LabelDiffusionTransformation();
+		default:
+			throw new RuntimeException("invalid transformation");
+		}
+		module.runOnOneImage(state, index);
+	}
 }
 
