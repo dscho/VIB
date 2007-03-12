7ad6b6ede5755bebbebcc4780e807b09357fe087
Johannes Schindelin <johannes.schindelin@gmx.de>
2007-03-12 03:45:58 +0100

    Readd EndModule
    I rethought the idea to include EndModule's functionality in
    Module, the base class of all modules. I am now convinced that
    it does not separate concepts enough.

---
+++ vib/app/module/EndModule.java
@@ -0,0 +1,16 @@
+package vib.app.module;
+
+import vib.app.gui.Console;
+
+public class EndModule extends Module {
+	protected static final String name = "EndModule";
+	protected static final String message = "Running the VIB protocol";
+
+	protected static void run(State state, int index) {
+		if (console == null)
+			console = Console.instance();
+		if (index != 0)
+			return;
+		AverageBrain.runOnAllImages(state);
+	}
+}
--- vib/app/module/Module.java
+++ vib/app/module/Module.java
@@ -4,14 +4,10 @@ import vib.app.gui.Console;
 
 public class Module {
 	protected static Console console;
-	protected static String name = "EndModule";
+	protected static String name, message;
 
 	protected static void run(State state, int index) {
-		if (console == null)
-			console = Console.instance();
-		if (index != 0)
-			return;
-		AverageBrain.runOnAllImages(state);
+		throw new RuntimeException("programming error");
 	}
 
 	// at a later stage, these functions will schedule multi-threaded jobs
