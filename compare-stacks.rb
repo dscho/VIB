a3026d43bc20c0e319cd3e5cfcd3bf2c60afe5d6
Mark Longair <mhl@pobox.com>
2008-02-25 14:11:26 +0000

    A small Ruby script to simplify comparing stacks from the command line.

---
+++ compare-stacks.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/ruby -w
+
+# It's a bit painful getting the escaping right for doing
+# this from the shell, so this is a small helper program.
+
+vib_directory=File.dirname(File.expand_path(__FILE__))
+
+memory="512m"
+
+unless ARGV.length == 2
+	puts "Usage: compare-stacks <fileA> <fileB>"
+	exit( -1 )
+end
+
+fileA=ARGV[0]
+fileB=ARGV[1]
+
+unless FileTest.exist? fileA
+	puts "File '#{fileA}' does not exist."
+	exit( -1 )
+end
+
+unless FileTest.exist? fileB
+	puts "File '#{fileB}' does not exist."
+	exit( -1 )
+end
+	
+fileA=File.expand_path(fileA)
+fileB=File.expand_path(fileB)
+
+Dir.chdir( vib_directory ) {
+
+	result = system( "java", "-Xmx#{memory}", "-Dplugins.dir=.", "-jar", "../ImageJ/ij.jar", fileA, fileB, "-eval", "run('Overlay Registered','');" )
+	unless result
+		puts "Running ImageJ failed."
+	end
+}
+
