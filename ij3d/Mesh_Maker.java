248bc81cd1be0413af867d6545a917fa28bce914
Albert Cardona <acardona@ini.phys.ethz.ch>
2008-12-11 17:23:37 +0100

    Added more example operations to Mesh_Maker.java

--- ij3d/Mesh_Maker.java
+++ ij3d/Mesh_Maker.java
@@ -9,6 +9,7 @@ package ij3d;
 import ij.plugin.PlugIn;
 import javax.vecmath.Color3f;
 import javax.vecmath.Point3f;
+import javax.media.j3d.Transform3D;
 import java.util.List;
 import java.util.ArrayList;
 import java.awt.Color;
@@ -37,7 +38,7 @@ public class Mesh_Maker implements PlugIn {
 		double[] px = new double[]{100, 230, 320, 400};
 		double[] py = new double[]{100, 120, 230, 400};
 		double[] pz = new double[]{100, 200, 300, 400};
-		double[] pr = new double[]{ 5,  10, 10, 15, 10};
+		double[] pr = new double[]{ 10,  15,  40,  110};
 		Color3f colort = new Color3f(Color.yellow);
 
 		//define a second tube as a curving spiral in space
@@ -57,10 +58,29 @@ public class Mesh_Maker implements PlugIn {
 
 		// Add both spheres and the tubes
 		//     Last parameter is the resampling (1 means no resampling)
-		univ.addMesh(createSphere(x1, y1, z1, r1, 12, 12), color1, "Sphere 1", 1);
-		univ.addMesh(createSphere(x2, y2, z2, r2, 12, 12), color2, "Sphere 2", 1);
-		univ.addMesh(createTube(px, py, pz, pr, 12, false), colort, "Tube", 1);
-		univ.addMesh(createTube(px2, py2, pz2, pr2, 12, false), color_t2, "Tube spiral", 1);
+		Content sph1 = univ.addMesh(createSphere(x1, y1, z1, r1, 12, 12), color1, "Sphere 1", 1);
+		Content sph2 = univ.addMesh(createSphere(x2, y2, z2, r2, 12, 12), color2, "Sphere 2", 1);
+		Content tube1 = univ.addMesh(createTube(px, py, pz, pr, 12, false), colort, "Tube", 1);
+		Content tube2 = univ.addMesh(createTube(px2, py2, pz2, pr2, 12, false), color_t2, "Tube spiral", 1);
+
+
+		// Extra:
+		// Now modify some attributes:
+
+		// Lock the transformation of the spiral:
+		// so it can't be rotated relative to its center when selected
+		// Try it: when selecting the spiral and rotating it with the mouse,
+		// the whole view will rotate, not the spiral relative to itself.
+		tube2.toggleLock();
+
+		// Change the transparency of the first tube
+		tube1.setTransparency(0.5f); // from 0 (opaque) to 1 (fully transparent)
+
+		// Move the small sphere in x,y,z to a new location
+		sph1.applyTransform(new Transform3D(new double[]{1, 0, 0,  50,
+			                                         0, 1, 0, 100,
+			                                         0, 0, 1, -50,
+			                                         0, 0, 0,   1 }));
 	}
 
 	static public List createSphere(final double x, final double y, final double z, final double r) {
