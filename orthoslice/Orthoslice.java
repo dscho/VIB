84a30c940f726cfa4e47c29826a30162a39b6af1
Benjamin Schmid <bene.schmid@gmail.com>
2009-01-14 18:08:24 +0100

    API doc for Orthoslice.

--- orthoslice/Orthoslice.java
+++ orthoslice/Orthoslice.java
@@ -11,13 +11,38 @@ import java.util.BitSet;
 
 import voltex.VolumeRenderer;
 
+/**
+ * Orthoslice extends VolumeRenderer and modifies it in a way so that
+ * not the whole volume is displayed, but only three orthogonal slices
+ * through the volume.
+ * 
+ * @author Benjamin Schmid
+ */
 public class Orthoslice extends VolumeRenderer {
 
-	int[] slices = new int[3];
-	int[] dimensions = new int[3];
-	boolean[] visible = new boolean[3];
-	BitSet whichChild = new BitSet(6);
+	/** The indices of the currently displayed slices */
+	private int[] slices = new int[3];
 
+	/** The dimensions in x-, y- and z- direction */
+	private int[] dimensions = new int[3];
+
+	/** Flag indicating which planes are visible */
+	private boolean[] visible = new boolean[3];
+
+	/** The visible children of the axis Switch in VolumeRenderer */
+	private BitSet whichChild = new BitSet(6);
+
+	/**
+	 * Initializes a new Orthoslice with the given image, color, transparency
+	 * and channels. By default, the slice indices go through the center
+	 * of the image stack.
+	 * 
+	 * @param img The image stack
+	 * @param color The color this Orthoslice should use
+	 * @param tr The transparency of this Orthoslice
+	 * @param channels A boolean[] array which indicates which color channels
+	 * to use (only affects RGB images). The length of the array must be 3.
+	 */
 	public Orthoslice(ImagePlus img, Color3f color, 
 					float tr, boolean[] channels) {
 		super(img, color, tr, channels);
@@ -32,6 +57,12 @@ public class Orthoslice extends VolumeRenderer {
 		}
 	}
 
+	/**
+	 * Overwrites loadAxis() in VolumeRenderer to show only one plane
+	 * in each direction.
+	 * @param axis Must be one of X_AXIS, Y_AXIS or Z_AXIS in
+	 * VolumeRendConstants.
+	 */
 	@Override
 	protected void loadAxis(int axis) {
 
@@ -41,26 +72,40 @@ public class Orthoslice extends VolumeRenderer {
 		loadAxis(axis, i, front, back);
 	} 
 
+	/**
+	 * Override eyePtChanged() in VolumeRenderer to always show all
+	 * slices.
+	 * @param view
+	 */
 	@Override
 	public void eyePtChanged(View view) {
 		axisSwitch.setWhichChild(Switch.CHILD_MASK);
 		axisSwitch.setChildMask(whichChild);
 	}
 
-	public void setSlices(int[] v) {
-		for(int i = 0; i < 3; i++)
-			if(slices[i] != v[i])
-				setSlice(i, v[i]);
-	}
-
+	/**
+	 * Returns the current index of the specified plane
+	 * @param axis
+	 * @return
+	 */
 	public int getSlice(int axis) {
 		return slices[axis];
 	}
 
+	/**
+	 * Returns whether the specified plane is visible at the moment
+	 * @param axis
+	 * @return
+	 */
 	public boolean isVisible(int axis) {
 		return visible[axis];
 	}
 
+	/**
+	 * Sets the specified plane visible.
+	 * @param axis
+	 * @param b
+	 */
 	public void setVisible(int axis, boolean b) {
 		if(visible[axis] != b) {
 			visible[axis] = b;
@@ -70,14 +115,27 @@ public class Orthoslice extends VolumeRenderer {
 		}
 	}
 
+	/**
+	 * Decreases the index of the specified plane by one.
+	 * @param axis
+	 */
 	public void decrease(int axis) {
 		setSlice(axis, slices[axis]-1);
 	}
 
+	/**
+	 * Increases the index of the specified plane by one.
+	 * @param axis
+	 */
 	public void increase(int axis) {
 		setSlice(axis, slices[axis]+1);
 	}
 
+	/**
+	 * Sets the slice index of the specified plane to the given value.
+	 * @param axis
+	 * @param v
+	 */
 	public void setSlice(int axis, int v) {
 		if(v >= dimensions[axis] || v < 0)
 			return;
