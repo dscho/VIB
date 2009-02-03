4542bfc1b0f80ab31a79d83f34a916c216316ce5
Mark Longair <mhl@pobox.com>
2009-02-03 12:46:47 +0000

    Remove unnecessary elements of RegistrationAlgorithm

--- vib/oldregistration/RegistrationAlgorithm.java
+++ vib/oldregistration/RegistrationAlgorithm.java
@@ -6,7 +6,6 @@ import ij.ImagePlus;
 import ij.IJ;
 import ij.WindowManager;
 
-import vib.transforms.OrderedTransformations;
 import util.BatchOpener;
 import util.FileAndChannel;
 
@@ -14,13 +13,22 @@ public abstract class RegistrationAlgorithm {
 	
 	public boolean keepSourceImages;
 	public ImagePlus[] sourceImages;
-	public OrderedTransformations transformation;
 	
 	public ImagePlus getTemplate() {
 		return sourceImages[0];
 	}
 	
 	public ImagePlus getDomain() {
+		return sourceImages[1];
+	}
+
+	// The same functionality according to different terminology:
+
+	public ImagePlus getModel() {
+		return sourceImages[0];
+	}
+
+	public ImagePlus getFloating() {
 		return sourceImages[1];
 	}
 	
@@ -34,68 +42,5 @@ public abstract class RegistrationAlgorithm {
 		sourceImages[0]=f0imps[f0.getChannelZeroIndexed()];
 		sourceImages[1]=f1imps[f1.getChannelZeroIndexed()];
 	}
-	
-	public void loadImagesOld( FileAndChannel f0, FileAndChannel f1 ) {
-		
-		transformation=null;
-		
-		String fileTemplate=f0.getPath();
-		String fileDomain=f1.getPath();
-		
-		sourceImages=new ImagePlus[2];
-		keepSourceImages=true;
-		
-		ImagePlus[] toClose=new ImagePlus[256];
-		int imagesToClose=0;
-		
-		IJ.openImage(fileTemplate);
-		
-		int[] wList = WindowManager.getIDList();
-		if (wList==null) {
-			IJ.error("Bookstein_FromMarkers.produceOverlayed(): No images are open after loading template");
-			return;
-		}
-		
-		for (int i=0; i<wList.length; i++) {
-			ImagePlus imp = WindowManager.getImage(wList[i]);
-			if(f0.correctFileName(imp)) {
-				if(f0.correctChannel(imp))
-					sourceImages[0]=imp;
-				else
-					toClose[imagesToClose++]=imp;
-			}
-		}
-		
-		for(int i=0;i<imagesToClose;++i)
-			toClose[i].close();
-		
-		imagesToClose=0;
-		
-		System.gc();
-		
-		IJ.openImage(fileDomain);
-		
-		wList = WindowManager.getIDList();
-		if (wList==null) {
-			IJ.error("Bookstein_FromMarkers.produceOverlayed(): No images are open after loading domain");
-			return;
-		}
-		
-		for (int i=0; i<wList.length; i++) {
-			ImagePlus imp = WindowManager.getImage(wList[i]);
-			if(f1.correctFileName(imp)) {
-				if(f1.correctChannel(imp))
-					sourceImages[1]=imp;
-				else
-					toClose[imagesToClose++]=imp;
-			}
-		}
-		
-		for(int i=0;i<imagesToClose;++i)
-			toClose[i].close();
-		
-		System.gc();
-		
-	}
-	
+
 }
