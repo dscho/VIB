89f5c0390a8d21a1d10cfebd23668fbb05de63f5
Mark Longair <mhl@pobox.com>
2009-04-09 09:47:28 +0100

    Name_Points: remove some old comments and add GPL boilerplate and copyrights

--- landmarks/Name_Points.java
+++ landmarks/Name_Points.java
@@ -1,5 +1,27 @@
 /* -*- mode: java; c-basic-offset: 8; indent-tabs-mode: t; tab-width: 8 -*- */
 
+/* Copyright 2006, 2007, 2008, 2009 Mark Longair */
+/* Copyright 2008, 2009 Gregory Jefferis */
+
+/*
+  This file is part of the ImageJ plugin "Name Landmarks and Register".
+
+  The ImageJ plugin "Name Landmarks and Register" is free software;
+  you can redistribute it and/or modify it under the terms of the GNU
+  General Public License as published by the Free Software Foundation;
+  either version 3 of the License, or (at your option) any later
+  version.
+
+  The ImageJ plugin "Name Landmarks and Register" is distributed in
+  the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+  even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package landmarks;
 
 import ij.*;
@@ -32,14 +54,6 @@ import util.Penalty;
 import vib.FastMatrix;
 import vib.oldregistration.RegistrationAlgorithm;
 import util.Overlay_Registered;
-
-/* FIXME:
-
-     - scale the parameters to that i can set scbd to 1.0
-     - indicate where the point would be moved to
-     - add a key
-     - why do some really good looking alignments score apparently so low?
-*/
 
 class PointsDialog extends Dialog implements ActionListener, WindowListener {
 
