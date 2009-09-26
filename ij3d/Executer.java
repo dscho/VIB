f4a63b061f8a3c61bac32f339d6e3303c9c92b09
Benjamin Schmid <bene.schmid@gmail.com>
2009-09-26 13:46:21 +0200

    Fix wrong macro recording.
    it must be 'call("ij3d.ImageJ3DViewer", ...)' instead of
    'Image3DViewer'.

--- ij3d/Executer.java
+++ ij3d/Executer.java
@@ -1215,21 +1215,21 @@ public class Executer {
 	 * Recording methods
 	 * *********************************************************/
 	private static void record(String command) {
-		command = "ij3d.Image3DViewer." + command;
+		command = "ij3d.ImageJ3DViewer." + command;
 		if(ij.plugin.frame.Recorder.record) {
 			ij.plugin.frame.Recorder.record("call", command);
 		}
 	}
 	
 	private static void record(String command, String a1) {
-		command = "ij3d.Image3DViewer." + command;
+		command = "ij3d.ImageJ3DViewer." + command;
 		if(ij.plugin.frame.Recorder.record) {
 			ij.plugin.frame.Recorder.record("call", command, a1);
 		}
 	}
 
 	private static void record(String command, String a1, String a2) {
-		command = "ij3d.Image3DViewer." + command;
+		command = "ij3d.ImageJ3DViewer." + command;
 		if(ij.plugin.frame.Recorder.record) {
 			ij.plugin.frame.Recorder.record("call",command,a1,a2);
 		}
@@ -1237,7 +1237,7 @@ public class Executer {
 
 	private static void record(String command, String a1, 
 						String a2, String a3) {
-		command = "call(\"ij3d.Image3DViewer." + command + "\", " + 
+		command = "call(\"ij3d.ImageJ3DViewer." + command + "\", " + 
 				"\"" + a1 + "\"," + 
 				"\"" + a2 + "\"," + 
 				"\"" + a3 + "\")"; 
@@ -1247,7 +1247,7 @@ public class Executer {
 	}
 
 	private static void record(String command, String[] args) {
-		command = "call(\"ij3d.Image3DViewer." + command + "\"";
+		command = "call(\"ij3d.ImageJ3DViewer." + command + "\"";
 		for(int i = 0; i < args.length; i++) {
 			command += ", \"" + args[i] + "\"";
 		}
